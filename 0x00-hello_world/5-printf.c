0cf2b7a551aac3a966ff368e4ed109efbbe772b3
realelkingsley <kc198070@gmail.com>
2022-09-10 05:09:30 -0700

    fixed a typo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
-*main-entry point for the programm.
-*
-*return:0 if no errors, return non zero if non errors
-*/
+ *main-entry point for the programm.
+ *
+ *return:0 if no errors, return non zero if non errors
+ */
 int main(void)
 {
 printf("with proper grammer, but the outcome is a piece of art,\n");
