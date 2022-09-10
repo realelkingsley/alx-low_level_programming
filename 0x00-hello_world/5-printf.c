30f9f1a7b4b1547536a4ce9310db4b047d4f51a5
realelkingsley <kc198070@gmail.com>
2022-09-10 05:22:26 -0700

    fixed a typo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-
 /**
  *main-entry point for the programm.
  *
  *return:0 if no errors, return non zero if non errors
  */
-
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
