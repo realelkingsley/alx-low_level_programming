74df33a2391c8b68fa3f65a3b6bcdcd2cb9590c7
realelkingsley <kc198070@gmail.com>
2022-09-10 02:49:03 -0700

    uwa_nkeaa_self

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+*main-entry point for the programm.
+*
+*return:0 if no errors, return non zero if non errors
+*/
+init main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
+
