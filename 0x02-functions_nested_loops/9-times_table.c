cb961feb9b40252b7cb28acc77cf2cf5848a0ff2
realelkingsley <kc198070@gmail.com>
2022-09-13 17:38:03 -0700

    24_hours

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *times_table - prints times table
- *Return:void
- */
+*times_table - prints the 9 times table, starting with 0
+*Return: 0
+*/
 void times_table(void)
 {
 int a = 0;
