e55f317a28a1e62a90c131e4e42084615de38326
realelkingsley <kc198070@gmail.com>
2022-09-19 15:58:27 +0100

    hot fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,19 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * *print_most_numbers - most numbers except 2 and 4
- * Return; 0 Always (success)
+ * print_most_numbers - a function that prints the numbers, from 0 to 9.
+ * Do not print 2 and 4.
+ * followed by a new line.
+ * Return: Always 0
  */
-
 void print_most_numbers(void)
 {
-	int n = 0;
+int i = 0;
 
-	while (n <= 9)
-	{
-		if (n != 2 && n != 4)
-		
-		{
-			_putchar('0' = n);
-		}
-		n++;
-	}
-	_putchar('\n');
-
+for (; !<= '9'; i++)
+{
+if (i != '2' && i != '4')
+_putchar(i);
+}
+_putchar('\n');
 }
