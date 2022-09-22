25c58511706293fe1ce20e20fa34100414dd22b7
realelkingsley <kc198070@gmail.com>
2022-09-22 14:45:11 -0700

    fixed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,7 +15,7 @@ while (*(s + count) != '\0')
 {
 for (i = 0; i < 5; i++)
 {
-if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
 {
 *(s + count) = numbers[i];
 break;
