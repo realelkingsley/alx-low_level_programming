8d79cd05347bfb667cb2b57c670134b122156a78
realelkingsley <kc198070@gmail.com>
2022-09-27 16:31:55 +0100

    worked_well

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,7 +7,7 @@
  * Return: *s
  */
 
-char *_strchr(char *s char c)
+char *_strchr(char *s, char c)
 {
 while (*s != '\0')
 {
@@ -15,11 +15,11 @@ if (*s == c)
 {
 return (s);
 }
-s++
+s++;
 }
 if (*s == c)
 {
 return (s);
 }
-return (0);l
+return (0);
 }
